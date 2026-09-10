3cce61553a96d5bd5c2f4410c2233c7278dbe83b
rafael068574-ops <rafael068574@gmail.com>
2026-09-09 22:16:51 -0300

    atualizações de informações e mudanças nos sistemas de login e localstorage

--- curso-react/src/pages/Auth.jsx
+++ curso-react/src/pages/Auth.jsx
@@ -1,38 +1,49 @@
-import { useState } from "react"
-import { href, Link } from "react-router"
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
 
 function Auth() {
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
-    const [mensagem, setMensagem] = useState("");
+    const [mensagem, setMsg] = useState("");
 
-    function hanleLogin(){
+    const nav = useNavigate()
+
+    function hanleLogin() {
         const users = JSON.parse(localStorage.getItem('users'))
-  
 
-        let user = users.find(u =>{
+
+        let user = users.find(u => {
             return u.email == email
         })
 
-        if(!user){
-            //os estudantes irão fazer
-            return
+        if (!user) {
+            setMsg("Usuário não encontrado.");
+            return;
+
         }
 
-        if(user.senha == pass){
-            <a href="painel.jsx">Entrar</a>
-        }else{
-            setMensagem("ola, somos da terra")
+        if(user.password == senha){
+            setMsg("Login realizado com sucesso.");
+            localStorage.setItem(
+                'logged',
+                JSON.stringify(user)
+            );
+
+            nav('/painel')
+
+        } else {
+            setMsg("Senha incorreta.");
         }
     }
 
 
     return (
-        
+
         <div className="w-full h-full bg-dark">
+            {mensagem}
             <div className="fixed flex top-0 right-0 bottom-0 left-0  p-10 items-center justify-center z-50">
                 <div className="w-1/3 mx-auto my-auto p-4 bg-blue-100 rounded-lg shadow-lg flex flex-col houver:bg-dark">
-                    <Link to="/" class="mt-5 bg-red text-white text-center rounded-md py-2 cursor-pointer">Voltar</Link>
+                    <Link to="/" className="mt-5 bg-red text-white text-center rounded-md py-2 cursor-pointer">Voltar</Link>
 
                     <form className="flex flex-col">
                         <span className="text-left py-3">Email: </span>
@@ -50,7 +61,7 @@ function Auth() {
                             placeholder="Digite a sua senha"
                             onChange={(e) => setSenha(e.target.value)}
                         />
-                        <a onClick={hanleLogin} class="mt-5 bg-green text-white text-center rounded-md py-2 cursor-pointer">Entrar</a>
+                        <a onClick={hanleLogin} className="mt-5 bg-green text-white text-center rounded-md py-2 cursor-pointer">Entrar</a>
                     </form>
                 </div>
             </div>
--- curso-react/src/pages/Painel.jsx
+++ curso-react/src/pages/Painel.jsx
@@ -1,25 +1,35 @@
-import { useState } from 'react';
-import { Link } from 'react-router';
-
+import { useEffect, useState } from 'react';
+import {Link} from 'react-router';
 
 function Painel() {
     const [modal, setModal ] = useState(false) //bollean
     const [users, setUsers] = useState([]) //vetor
     const [user, setUser] = useState({}) //objeto
+    const [logged, setLogged ] = useState({})
     
+    useEffect(()=>{
+        const usersTemp = JSON.parse(localStorage.getItem('users'))
+        if(usersTemp) setUsers (usersTemp)
+    },[])
+    
+    function updateUser(pUser){
+        setModal(true)
+        setUser(pUser)
+    }
 
 
-function handleRegister(){
-    const newUsers = [...users, user]
-    setUsers(newUsers)
-    localStorage.setItem('users', JSON.stringify(newUsers))
-    setUser({})
-    setModal(false)
 
-}
+    function hanleRegister(){
+        const newUsers = [...users, user]
+        setUsers(newUsers)
+        localStorage.setItem('users', JSON.stringify(newUsers))
+        setUser({})
+        setModal(false)
+
+    }
     return (
  <>
-    <h3>Hi</h3>
+    <h3>Bem vindo,{logged?.nome}</h3>
 
 { modal && (
     <div
@@ -34,11 +44,11 @@ function handleRegister(){
 
             <form className="flex flex-col">
 
-                Nome: <input onChange={ (e) => setUser({...user, nome: e.target.value }) } type="text" placeholder="Digite seu nome completo" />
-                Email: <input onChange={ (e) => setUser({...user, email: e.target.value }) } type="email" placeholder="Digite o seu melhor email" />
-                Senha: <input onChange={ (e) => setUser({...user, password: e.target.value }) } type="password" placeholder="Letra maiuscula e números" />
-                Data de Nascimento: <input onChange={ (e) => setUser({...user, date: e.target.value }) } type="date"/>
-                <a onClick={handleRegister} className="mr-3 py-2 px-2 hover:bg-dark hover:text-primary rounded ml-auto py-2 shadow 3px bottom-0 cursor-pointer mx-auto bg-dark">Salvar</a>
+                Nome: <input value={user.nome} onChange={ (e) => setUser({...user, nome: e.target.value }) } type="text" placeholder="Digite seu nome completo" />
+                Email: <input value={user.email} onChange={ (e) => setUser({...user, email: e.target.value }) } type="email" placeholder="Digite o seu melhor email" />
+                Senha: <input value={user.password} onChange={ (e) => setUser({...user, password: e.target.value }) } type="password" placeholder="Letra maiuscula e números" />
+                Data de Nascimento: <input value={user.date} onChange={ (e) => setUser({...user, date: e.target.value }) } type="date"/>
+                <a onClick={hanleRegister} className="mr-3 py-2 px-2 hover:bg-dark hover:text-primary rounded ml-auto py-2 shadow 3px bottom-0 cursor-pointer mx-auto bg-dark">Salvar</a>
             </form>
 
         </div>
@@ -51,7 +61,34 @@ function handleRegister(){
             <th>Ações</th>
         </thead>
 
-        <tbody id="listU" className="font-secondary">
+        <tbody className="font-secondary">
+            {users.map( u => (
+                <tr>
+                    <td>{u.nome}</td>
+                    <td>{u.email}</td>
+                    <td>
+                        <a className='cursor-pointer
+                        px-2
+                        mx-4
+                        hover:shadow
+                        shadow-md
+                        text-white
+                        rounded-full
+                        bg-green-500'
+                    onClick={()=> updateUser(u)}
+                        >V</a>
+                        <a className='cursor-pointer
+                        px-2
+                        mx-4
+                        hover:shadow
+                        shadow-md
+                        text-white
+                        rounded-full
+                        bg-red-500'
+                        >X</a>
+                    </td>
+                </tr>
+            ))}
             
         </tbody>
     </table>
